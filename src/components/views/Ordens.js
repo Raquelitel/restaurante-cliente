c595785570b51d19aef67c9cb8c9101ad4af49c7
Raquel <raquel@MacBook-Pro-de-Raquel.local>
2022-12-20 16:58:06 +0000

    backlog change time order

--- src/components/views/Ordens.js
+++ src/components/views/Ordens.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { FirebaseContext } from '../../firebase'
+import Order from '../UI/Order';
 
 const Ordens = () => {
+
+  const { firebase } = useContext(FirebaseContext);
+  const [ordenes, setOrdenes] = useState([])
+
+  useEffect(() => {
+    const getOrders = () => {
+      firebase.db.collection("ordenes").where("completado", "==", false).onSnapshot(handleSnapshot);
+    }
+
+
+    getOrders();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const ordenes = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    setOrdenes(ordenes)
+  }
+
+
   return (
-    <div>Ordens</div>
+    <>
+      <h1 className='text-3xl font-light mb-4'>Ordens</h1>
+      <div className='sm:flex sm:flex-wrap -mx-3'>
+        {ordenes.map(orden => (
+          <Order
+            key={orden.id}
+            orden={orden}
+          />
+        ))}
+      </div>
+
+    </>
+
   )
 }
 
